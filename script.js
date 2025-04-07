364ebd581c97e1a383ff89a41e9a82594c6c47f4
MaungMyatAung <85542468+MaungMyatAung@users.noreply.github.com>
2025-04-07 08:28:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 // Your script here.
+// Set the utterance's text to the textarea's value
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = speechSynthesis.getVoices();
+  voicesDropdown.innerHTML = voices
+    .filter(voice => voice.lang.includes('en')) // Only show English voices
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+
+// Set voice from dropdown
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name === this.value);
+  toggle(); // Restart with new voice
+}
+
+// Set pitch/rate/text values
+function setOption() {
+  msg[this.name] = this.value;
+}
+
+// Speak the message
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) speechSynthesis.speak(msg);
+}
+
+// Events
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
+
